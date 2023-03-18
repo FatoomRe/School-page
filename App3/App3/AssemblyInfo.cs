add4305396cef92ea269e054cbf2b21f91d787e1
Fatimah <Nyzak1@outlook.sa>
2023-03-18 10:53:28 +0300

    uplod in github

---
+++ App3/App3/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+[assembly: ExportFont("Font Awesome 6 Free-Solid-900.otf", Alias = "FontAwesome")]
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
